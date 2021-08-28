e5a29ed2350cba03b8bde8bc23a0230e2ede5a7c
Miguel Angel Fuquene Arias <migueplay_10@hotmail.com>
2021-08-28 08:11:37 -0500

    Files generated in point 1

---
+++ src/main/java/edu/eci/ieti/taskapirest/taskdto/TaskdtoApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.taskapirest.taskdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/taskapirest/taskdto/TaskdtoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.taskapirest.taskdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
